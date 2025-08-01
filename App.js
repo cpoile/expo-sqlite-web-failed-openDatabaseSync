f2c93ffb0a52d6fb4c5df00892d54037a6f3a7b1
mjrt <mjrt.dev@outlook.com>
2025-08-01 16:20:13 +0800

    feat: reproduce openDatabaseSync issue

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { SQLiteProvider } from 'expo-sqlite';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-
-
+import { SQLiteProvider } from "expo-sqlite";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { openDatabaseSync } from "expo-sqlite";
 
 export default function App() {
+  const conn = openDatabaseSync("test.db");
+
   return (
     <View style={styles.container}>
-      <SQLiteProvider databaseName="test.db">
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </SQLiteProvider>
+      {/* <SQLiteProvider databaseName="test.db"> */}
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      {/* </SQLiteProvider> */}
     </View>
   );
 }
@@ -19,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
